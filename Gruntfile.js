6dd1a503f97f1b3bcd2614cb26c0cc8d7b390643
Lars Thorup <lars@zealake.com>
2013-04-18 13:40:44 -0600

    Ready for presentation

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function (grunt) {
+    var gruntConfig = {};
+
+    // lint
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    gruntConfig.jshint = {
+        all: [
+            '*.js',
+            'src/**/*.js'
+        ]
+    };
+
+    // test
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    gruntConfig.jasmine = {
+        src: {
+            src: [
+                'src/js/**/*.js'
+            ],
+            options: {
+                specs: 'src/test/**/*.test.js',
+                junit: {
+                    path: 'output/testresults'
+                }
+            }
+        }
+    };
+
+    // coverage
+    gruntConfig.jasmine.istanbul = {
+        src: gruntConfig.jasmine.src.src,
+        options: {
+            specs: gruntConfig.jasmine.src.options.specs,
+            template: require('grunt-template-jasmine-istanbul'),
+            templateOptions: {
+                coverage: 'output/coverage/coverage.json',
+                report: [
+                    {type: 'html', options: {dir: 'output/coverage'}},
+                    {type: 'cobertura', options: {dir: 'output/coverage/cobertura'}},
+                    {type: 'text-summary'}
+                ]
+            }
+        }
+    };
+
+    // grunt
+    grunt.initConfig(gruntConfig);
+};
